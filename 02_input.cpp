6c3ad4336417795585744d16cdd481b800edeb3c
sarthak <you@example.com>
2025-07-10 16:35:52 +0530

    input handling

---
+++ 02_input.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //input in c++
+    
+
+    // Q.perform operations using operators
+
+    // int amount1;
+    // cout << "Enter the amount1: ";
+    // cin >> amount1;
+
+    // int amount2;
+    // cout << "Enter the amount2: ";
+    // cin >> amount2;
+
+    // int sum = amount1 + amount2;
+    // int sub = amount1 - amount2;
+    // int mul = amount1 * amount2;
+    // double div = amount1 / amount2;
+    // double modulus = amount1 % amount2;
+
+    // cout << "Sum of two numbers = " << sum << endl;
+    // cout << "Subtraction of two numbers = " << sub << endl;
+    // cout << "Multiplication of two numbers = " << mul << endl;
+    // cout << "Division of two numbers = " << div << endl;
+    // cout << "Modulus of two numbers = " << modulus << endl;
+
+    // unary operator
+
+    // int a = 20;
+    // int b = a++; //increment operator - 20 (kaam; update)
+    // int c = --a; //decrement operator - 19 (update; kaam)
+
+    // cout << "b = " << b << endl; //20
+    // cout << "a = " << a << endl; //21
+    
+    // cout << "c = " << c << endl; //19
+    // cout << "a = " << a << endl; //19
+
+    // cout << "Produces beep" << "\a";
+    return 0;
+}
