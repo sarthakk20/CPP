24e7e4862793251a32e24d5932c788a303487db8
sarthak <you@example.com>
2025-07-10 16:37:26 +0530

    conditional loop

---
+++ 03_ConditionalLoops.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //if-else
+
+    // int age;
+    // cout << "Enter Your age : ";
+    // cin >> age;
+
+    // if (age>18)
+    // {
+    //     cout << "You can vote";
+    // }
+    // else{
+    //     cout << "You can't vote";
+    // }
+
+    // Ternary operator
+
+    // int n;
+    // cout << "Enter number n : ";
+    // cin >> n;
+
+    // cout << (n>=0 ? "Yes" : "No");
+    
+    // while-loop
+
+    // Q.print number till 10
+
+    // int count = 1;
+
+    // while (count <= 10)
+    // {
+    //     cout << count <<endl;
+    //     count++;
+    // }
+
+    // Q. print table of any number.
+
+    // int num;
+    // int i=1;
+
+    // cout << "Enter any number to print table: ";
+    // cin >> num;
+
+    // while (i<=10)    
+    // {
+    //     int sum = num*i;
+    //     cout << num << " X " << i << " = " << sum << endl;
+    //     // cout << "\n";
+    //     i++;
+    // }
+
+    // For-loop
+
+    // int count;
+    // int sum = 0;
+    // cout << "Enter number: ";
+    // cin >> count;
+
+    // for (int i=0; i <= count; i++)
+    // {
+    //     sum = sum + i;
+    // }
+    // cout << sum;
+
+    // Q. Sum of odd numbers till n
+
+    // int n;
+    // int sum = 0;
+    // cout << "Enter number: ";
+    // cin >> n;
+
+    // for (int i = 0; i <= n; i++)
+    // {
+    //     if (i%2 != 0)
+    //     {
+    //         sum = sum + i;
+    //     }
+        
+    // }
+    // cout << "Sum of odd number is : " << sum << endl;
+
+    // Q. print if the number is prime or not
+
+    // int n = 7;
+    // bool isPrime = true;
+
+    // for (int i = 2; i <= n-1; i++)
+    // for (int i = 2; i*i <= n; i++)    //optimized approach
+    // {
+    //     if ( n%i == 0 ) //non-prime no
+    //     {
+    //         isPrime = false;
+    //         break;
+    //     }
+    // }
+
+    // if (isPrime)
+    // {
+    //     cout << "Prime number";
+    // }
+    // else{
+    //     cout << "Non-Prime number";
+    // }
+    
+    
+    // Do-while-loop
+
+    
+    
+
+    return 0;
+}
