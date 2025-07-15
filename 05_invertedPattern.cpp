9f8b2d46dbc7c1f3e2a579ffa1bab204fdfe7932
sarthak <you@example.com>
2025-07-15 12:19:46 +0530

    pyramid pattern

--- 05_invertedPattern.cpp
+++ 05_invertedPattern.cpp
@@ -48,6 +48,36 @@ int main(){
     //             B B B
     //               C C
     //                 D
+
+    //pyramid Pattern - number version
+
+    int n=4;
+
+    for (int i = 1; i <= 4; i++){
+
+        for (int j = 0; j < n-i; j++)
+        {
+            cout << "  ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j <<" ";
+        }
+        for (int j = i-1; j > 0; j--)
+        {
+            cout << j << " ";
+        }
+        
+        cout << endl;
+    }
+    
+
+    // output: 
+    //                1 
+    //              1 2 1
+    //            1 2 3 2 1
+    //          1 2 3 4 3 2 1
+
     return 0;
 
 }
