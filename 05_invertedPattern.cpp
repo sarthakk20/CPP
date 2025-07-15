ecc8c6db4467e4c343c25b71597a226edccfb849
sarthak <you@example.com>
2025-07-15 11:18:51 +0530

    inverted triangle character version

--- 05_invertedPattern.cpp
+++ 05_invertedPattern.cpp
@@ -5,7 +5,30 @@ int main(){
 
     // Inverted Triangle Pattern
 
+    // int n = 4;
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i; j++)
+    //     {
+    //         cout << "  ";
+    //     }
+    //     for (int j = 0; j < n-i; j++)
+    //     {
+    //         cout << i+1 << " ";
+    //     }
+    //     cout << endl;
+    // }
+    // output: 
+    //           1 1 1 1
+    //             2 2 2
+    //               3 3
+    //                 4
+
+    // Inverted Triangle Pattern - character version
+
     int n = 4;
+    char ch = 'A';
 
     for (int i = 0; i < n; i++)
     {
@@ -15,15 +38,16 @@ int main(){
         }
         for (int j = 0; j < n-i; j++)
         {
-            cout << i+1 << " ";
+            cout << ch << " ";
         }
+        ch++;
         cout << endl;
     }
     // output: 
-    //           1 1 1 1
-    //             2 2 2
-    //               3 3
-    //                 4
+    //           A A A A 
+    //             B B B
+    //               C C
+    //                 D
     return 0;
 
 }
