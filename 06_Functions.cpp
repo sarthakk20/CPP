3cb4ef845b3c42182dc98c417f7fffce67519766
sarthak <you@example.com>
2025-07-17 17:34:48 +0530

    basic functions

--- 06_Functions.cpp
+++ 06_Functions.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 using namespace std;
 
+
+    // int printFun(){
+    //     cout << "Learning functions" << endl;
+    // }
+
+    // int FactorialFun(int n){
+    //     int fact = 1;
+    //     for (int i = 1; i <= n; i++){
+    //         fact = fact * i;
+    //     }
+    //     return fact;
+    // }
+
+
 int main(){
 
-    // Functions : 
-    cout << "Learning functions" << endl;
+    // // Function invoke : 
+    // printFun();
+
+    // // Factorial of number
+    // int num = 5;
+    // cout << FactorialFun(5) << endl;
+
 
     return 0;
 }
