2cad1f39ea604adf12411577f48fd5ba99355b41
sarthak <you@example.com>
2025-07-18 18:05:35 +0530

    functions examples

--- 06_Functions.cpp
+++ 06_Functions.cpp
@@ -6,13 +6,22 @@ using namespace std;
     //     cout << "Learning functions" << endl;
     // }
 
-    // int FactorialFun(int n){
-    //     int fact = 1;
-    //     for (int i = 1; i <= n; i++){
-    //         fact = fact * i;
-    //     }
-    //     return fact;
-    // }
+    int FactorialFun(int n){
+        int fact = 1;
+        for (int i = 1; i <= n; i++){
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+    int BinoFun(int n, int m){
+
+        int fact_n = FactorialFun(n);
+        int fact_m = FactorialFun(m);
+        int fact_nm = FactorialFun(n-m);
+
+        return fact_n/(fact_m - fact_nm) ; 
+    }
 
 
 int main(){
@@ -23,6 +32,16 @@ int main(){
     // // Factorial of number
     // int num = 5;
     // cout << FactorialFun(5) << endl;
+
+    // Binomial Coefficient
+
+    int n = 5;
+    int m = 4;
+
+    cout <<"Binomial Coefficient is " << BinoFun(n , m);
+
+
+
 
 
     return 0;
