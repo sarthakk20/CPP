ed99e22157a9166941fed1e4bad7fcb1f6e8a94a
sarthak <you@example.com>
2025-07-19 09:05:22 +0530

    binary number conversion

---
+++ 07_BinaryNums.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // Decimal to Binary
+
+    // int dec = 15;
+    // int ans = 0;
+    // int pow = 1;
+    // int bin;
+
+    // while (dec > 0)
+    // {
+    //     bin = dec % 2;
+    //     dec = dec / 2;
+        
+    //     bin = bin * pow;
+    //     ans = ans + bin;
+    //     pow = pow * 10;
+    // }
+
+    // cout << "Binay form of 5 is :" << ans << endl;
+
+    // Binary to Decimal
+
+    int binNum = 1111;
+    int rem , mul;
+    int pow = 1;
+    int ans = 0;
+
+    while (binNum > 0) 
+    {
+        rem = binNum % 10;
+        binNum /= 10;
+
+        mul = rem * pow;
+        ans += mul;
+        pow *= 2;
+    }
+    
+    cout << "Decimal form of 1111 is : " << ans << endl;
+    
+    return 0;
+}
