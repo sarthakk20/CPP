873e42a7d554bd110d3a323d0d7053a594f3f508
Sarthak Sonawane <sarthak20.sonawane@gmail.com>
2025-07-19 12:33:22 +0530

    fibo function

--- 06_Functions.cpp
+++ 06_Functions.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 
-    // int printFun(){
-    //     cout << "Learning functions" << endl;
-    // }
+    int printFun(){
+        cout << "Learning functions" << endl;
+    }
 
     int FactorialFun(int n){
         int fact = 1;
@@ -23,6 +23,37 @@ using namespace std;
         return fact_n/(fact_m - fact_nm) ; 
     }
 
+    int sumOfDigit(int num){
+        int sum=0;
+
+        while (num > 0)
+        {
+            int lastDig = num % 10;
+            num /= 10;
+
+            sum += lastDig;
+        }
+        return sum;
+    }
+
+    int FibonacciSeries(int n){
+
+        int firVal = 0;
+        int secVal = 1;
+        int fiboSum = 0;
+        cout << firVal << endl;
+        cout << secVal << endl;
+
+        for (int i = 2; i < n; i++){
+            
+            fiboSum = firVal + secVal;
+            cout << fiboSum << endl;
+            firVal = secVal;
+            secVal = fiboSum;
+        }
+        return fiboSum;
+    }
+
 
 int main(){
 
@@ -33,16 +64,23 @@ int main(){
     // int num = 5;
     // cout << FactorialFun(5) << endl;
 
-    // Binomial Coefficient
+    // // Binomial Coefficient
 
-    int n = 5;
-    int m = 4;
+    // int n = 5;
+    // int m = 4;
 
-    cout <<"Binomial Coefficient is " << BinoFun(n , m);
+    // cout <<"Binomial Coefficient is " << BinoFun(n , m);
 
+    // // Sum Of Digit
 
+    // int num = 5555;
+    // cout << "Sum of Digits = " << sumOfDigit(num);
 
+    // //Fibonacci series
 
+    // int n = 8;
+
+    // cout << "Fibonacci Series upto n is = " << FibonacciSeries(n) << endl;
 
     return 0;
 }
