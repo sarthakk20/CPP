f2d9ad87be011bc83ff9c80a9f7df16d7e14df4b
Sarthak Sonawane <sarthak20.sonawane@gmail.com>
2025-07-21 12:23:40 +0530

    Solving a problem using bitwise operators

--- 07_BinaryNums.cpp
+++ 07_BinaryNums.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+bool isPowerOfTwo(int n) {
+    return (n > 0) && ((n & (n - 1)) == 0);
+}
+
 int main(){
 
     // Decimal to Binary
@@ -48,12 +52,25 @@ int main(){
     // Left shift bitwise operator[ << ]
     // (a << b) - a * 2^b
     
-    cout << (10 << 1) << endl; //20 = 10*2^1
-    cout << (10 << 2) << endl; //40 = 10*2^1
+    // cout << (10 << 1) << endl; //20 = 10*2^1
+    // cout << (10 << 2) << endl; //40 = 10*2^1
     
-    // Left shift bitwise operator[ >> ]
-    // (a >> b) - a / 2^b
-    cout << (10 >> 1) << endl; //5 = 10/2^1
-    cout << (10 >> 2) << endl; //2 = 10/2^2
+    // // Left shift bitwise operator[ >> ]
+    // // (a >> b) - a / 2^b
+    // cout << (10 >> 1) << endl; //5 = 10/2^1
+    // cout << (10 >> 2) << endl; //2 = 10/2^2
+
+
+    //Q. check if a number is a power of 2 without using loops by using bitwise operations. 
+
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (isPowerOfTwo(num))
+        cout << num << " is a power of 2." << endl;
+    else
+        cout << num << " is NOT a power of 2." << endl;
+    
     return 0;
 }
