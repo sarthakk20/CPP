4b5f0cef1d2105e041bd81aa25f8ce8e1efa6e62
Sarthak Sonawane <sarthak20.sonawane@gmail.com>
2025-07-22 10:54:34 +0530

    smallest/largest value

---
+++ DSA_01.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    /*ARRAY*/
+
+    // // Declaring variable nums with 9 value and initializing some values.
+    // int nums[9] = {32,34,67,88,78,77,99,100};
+    // nums[8]= 89;
+
+    // int size = sizeof(nums) / sizeof(int);
+    // cout << size << endl;
+
+    // // Looping array of nums
+    // for (int i = 0; i < size; i++)
+    // {
+    //     cout << "Num" << i << " = " << nums[i] << endl;
+    // }
+
+    // //Q.Find the minimum from the given array
+
+    // int arr[5] = {82, 76, 4, -9, 2};
+    // int size = sizeof(arr) / sizeof(int); 
+
+    // int smallest = INT16_MAX;  //this is the value of +infinity that will be always highest
+
+    // for (int i = 0; i < size; i++)
+    // {
+    //     // if (i < smallest){
+    //     //     smallest = arr[i];
+    //     // }
+    //     smallest = min(arr[i],smallest);
+        
+    // }
+    // cout << "Smallest value : " << smallest << endl;
+
+    // //Q.Find the maximum from the given array
+
+    // int arry[5] = {82, 76, 4, -9, 99};
+    // // int size = sizeof(arr) / sizeof(int); 
+
+    // int largest = INT16_MIN;  //this is the value of -infinity that will be always lowest
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     // if (arry[i] > largest){
+    //     //     largest = arry[i];
+    //     // }
+    //     largest = max(arry[i],largest);
+    // }
+    // cout << "Largest value : " << largest << endl;
+    
+
+    return 0;
+}
