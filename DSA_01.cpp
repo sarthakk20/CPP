1b1b525c5b2260b78a20b7fd26c8ea3a2627e866
Sarthak Sonawane <sarthak20.sonawane@gmail.com>
2025-07-22 11:29:31 +0530

    index smallest/largest value

--- DSA_01.cpp
+++ DSA_01.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 using namespace std;
 
+
+    int indexOfLargestVal(int arry[], int size){
+        int maxIndex = 0;
+        for (int i = 0; i < size; i++)
+        {
+            if (arry[i] > arry[maxIndex]){
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+    int indexOfSmallestVal(int arry[], int size){
+        int smallest = INT16_MAX; 
+        int minIndex;
+
+        for (int i = 0; i < size; i++)
+        {
+            if (i < smallest){
+                smallest = arry[i];
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+
 int main(){
 
     /*ARRAY*/
@@ -8,7 +33,6 @@ int main(){
     // // Declaring variable nums with 9 value and initializing some values.
     // int nums[9] = {32,34,67,88,78,77,99,100};
     // nums[8]= 89;
-
     // int size = sizeof(nums) / sizeof(int);
     // cout << size << endl;
 
@@ -38,7 +62,7 @@ int main(){
     // //Q.Find the maximum from the given array
 
     // int arry[5] = {82, 76, 4, -9, 99};
-    // // int size = sizeof(arr) / sizeof(int); 
+    // // int size = sizeof(arry) / sizeof(int); 
 
     // int largest = INT16_MIN;  //this is the value of -infinity that will be always lowest
 
@@ -50,6 +74,15 @@ int main(){
     //     largest = max(arry[i],largest);
     // }
     // cout << "Largest value : " << largest << endl;
+
+    // //Q.Find the index of maximum vlaue from the given array
+
+    int arry[5] = {82, 76, 4, -9, 99};
+    int size = sizeof(arry) / sizeof(int);
+    int index1 = indexOfLargestVal(arry, size);
+    int index2 = indexOfSmallestVal(arry, size);
+    cout << "Index of largest value " << arry[index1] << " is " << index1 << endl;
+    cout << "Index of smallest value " << arry[index2] << " is " << index2 << endl;
     
 
     return 0;
