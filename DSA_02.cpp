50fbfd9964d8fb7a3d0d6d295c14eafbecf2f778
Sarthak Sonawane <sarthak20.sonawane@gmail.com>
2025-08-02 11:34:08 +0530

    dsa problem 2

---
+++ DSA_02.cpp
@@ -0,0 +1,30 @@
+// #include <iostream>
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int removeElement(int val, vector <int> nums[]){
+//     vector <int> :: iterator k;
+//     k = nums.begin();
+//     for (int i = 0; i < nums[5]; i++)
+//     {
+//         if (nums[i] == val)
+//         {
+//             nums.erase(k);
+//             i--; // Decrement i to account for the removed element
+//             k--;
+//         }
+//         k++;
+//     }
+//     return nums.size();
+// }
+
+
+// int mai(){
+//     // removing element
+
+//     int nums[5]={1,2,2,3,4};
+//     int val = 2;
+
+//     cout << removeElement(val, nums);
+//     return 0;
+// }
